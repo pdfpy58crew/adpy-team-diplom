b848ef95e2b21317d6227cd49fd0727bb5773cd3
Andrey <bucksbunny.butovo@gmail.com>
2022-10-02 23:48:59 +0300

    logging bot_logger

--- vk_bot/bot_longpoll.py
+++ vk_bot/bot_longpoll.py
@@ -3,6 +3,7 @@ from random import randrange
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+from vk_bot.bot_logger import bot_logger
 
 token = 'vk1.a.GnI3SSl0PBNmukAiPtKyyvm9gbZ6NdE6YGilCCGrpmvIMg_uGwD4bGB61Y4NrvaTGryvSSg6nYxTVCys62ALhin0GNOGmhsGoJbpZk-l2cOMNdXnJeSWeYbcnFZFEKPTaHvji_75-inntsGgm32vackYy7E4pNWBKRpBXaoUISraiio7MkLaO8qNmum6gXSA'
 group_id = 216235876
@@ -35,26 +36,30 @@ class Bot:
         """Распаковка сообщения"""
         return event.obj['message']
 
+
     def _write_msg(self, text, attachment:str=None, keyboard=None):
         """Отправка сообщения"""
         response = vk.method('messages.send', {'user_id': self.user_id, 'message': text,  'random_id': randrange(10 ** 7), 'keyboard': keyboard, 'attachment': attachment,})
         return response
 
+    @bot_logger
     def start(self):
         """Сборка приветствия
         :return: id сообщения или код ошибки"""
         message = f"Хай, {self.user_id}, давай найдем тебе пару. Дави 'Найти', и испытай удачу!"
         keyboard = keyboard_ontime.get_keyboard
         response = self._write_msg(message, keyboard=keyboard())
-        return response
+        return [response, self.user_id]
 
+    @bot_logger
     def like_user(self):
         """Добавление в Избранное"""
         message = 'Добавлено в Избранное, продолжим?'
         # VK_api.like(user_id)
         response = self._write_msg(message)
-        return response
+        return [response, self.user_id]
 
+    @bot_logger
     def next_user(self):
         """Отправка анкеты претендента пользователю"""
         # user = VK_api.roll()
@@ -62,14 +67,15 @@ class Bot:
         attachment = 'photo1_376599151,photo1_456264771,photo1_263219735'
         keyboard = keyboard_inline.get_keyboard
         response = self._write_msg(message, attachment=attachment, keyboard=keyboard())
-        return response
+        return [response, self.user_id]
 
+    @bot_logger
     def favorite_list(self):
         """Получение списка Избранного"""
         # DB.get_favorites(self.user_id)
         message = "Список избранного..."
         response = self._write_msg(message)
-        return response
+        return [response, self.user_id]
 
     def __user_link(self, user_id:str):
         """конструктор ссылки на пользователя"""
