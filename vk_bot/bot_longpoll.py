10b8f6dbea423114c98b00468b7a4e4cc6942134
Andrey <bucksbunny.butovo@gmail.com>
2022-10-08 20:00:38 +0300

    opt

--- vk_bot/bot_longpoll.py
+++ vk_bot/bot_longpoll.py
@@ -12,8 +12,6 @@ import DB.dbconnection as db
 G_TOKEN = 'vk1.a.GnI3SSl0PBNmukAiPtKyyvm9gbZ6NdE6YGilCCGrpmvIMg_uGwD4bGB61Y4NrvaTGryvSSg6nYxTVCys62ALhin0GNOGmhsGoJbpZk-l2cOMNdXnJeSWeYbcnFZFEKPTaHvji_75-inntsGgm32vackYy7E4pNWBKRpBXaoUISraiio7MkLaO8qNmum6gXSA'
 G_ID = 216235876
 
-# vk = vk_api.VkApi(token=token)
-# longpoll = VkBotLongPoll(vk, group_id)
 
 ###KEYBOARDS###
 keyboard_ontime = VkKeyboard(one_time=False)
@@ -35,11 +33,6 @@ class Bot:
         self.user_info = None
         self.friends = []
         self.friend = None
-        # self.event = event.obj['message']
-        # self.text = self.event['text']
-        # self.user_id = self.event['from_id']
-        # self.is_keyboard = event.obj['client_info']['keyboard']
-        # self.actual_id = None
 
 
     def listen(self):
@@ -60,14 +53,18 @@ class Bot:
 
 
     def _write_msg(self, user_id, text, attachment:str=None, keyboard=None, forward_msg=None):
-        """Отправка сообщения"""
+        """Отправка сообщения
+        :return:str № отправленного сообщения или описание ошибки VK"""
         try:
             response = self.vk.method('messages.send', {'user_id': user_id, 'message': text,  'random_id': randrange(10 ** 7), 'keyboard': keyboard, 'attachment': attachment, 'forward_messages': forward_msg})
         except vk_api.exceptions.ApiError as error_msg:
             response = error_msg
         return response
 
+
     def _get_user(self, user_id):
+        """запрос данных пользователя. 
+        :return:str имя пользователя сообщения"""
         response = self.vk.method('users.get', {'user_ids': user_id})
         name = response[0]['first_name']
         return name
@@ -80,33 +77,48 @@ class Bot:
         name = self._get_user(user_id)
         message = f"""Хай, {name}, Я BTinder. Я найду любому одинокому и несчастному пару, мои алгоритмы помогают найти ее по максимальному совпадению с твоими интересами.\n
                     Если тебе кто-то понравится, добавь его в Избранное. Ты в любой момент сможешь получить доступ к списку понравившихся аккаунтов, нажав кнопку 'Избранное'\n
-                    Если тебе не нравится подобранный аккаунт - просто жми 'Найти'
+                    Если тебе не нравится подобранный аккаунт - просто жми 'Найти'\n
                     Готов? Тогда дави 'Найти', и испытай удачу!\n """
         keyboard = keyboard_ontime.get_keyboard()
         response = self._write_msg(user_id, message, keyboard=keyboard)
         return [response, user_id]
 
+
     @bot_logger
     def like_user(self, user_id):
-        """Добавление в Избранное"""
+        """Добавление в Избранное. Добавляет последнюю анкету в список избранного БД"""
         db.add_to_favorites(self.user_info, self.friend)
         message = f'Прекрасный выбор! {self.friend["first_name"]} {self.friend["last_name"]} уже добавлено в Избранное, продолжим?'
         response = self._write_msg(user_id, message)
         return [response, user_id]
 
+
     @bot_logger
     def next_user(self, user_id):
-        """Отправка анкеты претендента пользователю"""
+        """Отправка анкеты претендента пользователю:
+        - запрос данных пользователя
+        - добавление данных в БД
+        - отправка в ЧС текущей анкеты, если не был добавлен в избранное
+        - получение списка анкет, если таковой пуст
+        - нахождение 1й анкеты в списке не из ЧС и избранного и установка ее как текущей
+        - проверка на то что такая анкета нашлась
+        - отправка пользователю
+        - """
         self.user_info = self.vk_api.get_user_information()
         db.add_new_user(self.user_info)
+
         if self.friend and not db.check_favorites(self.friend['user_id']):
             db.add_to_blacklist(self.user_info, self.friend)
             self.friend = None
-        self.friends = self.vk_api.search_friends(self.user_info)
+
+        if not self.friends:
+            self.friends = self.vk_api.search_friends(self.user_info)
+
         for friend in self.friends:
             if not db.check_black_list(friend['user_id']) and not db.check_favorites(friend['user_id']):
                 self.friend = friend
                 break
+            
         if self.friend:
             message = f"{self.friend['first_name']} {self.friend['last_name']}\n{self.friend['user_link']}\n"
             attachment = self.__get_foto_list(self.friend['user_id'])
@@ -117,9 +129,13 @@ class Bot:
             response = self._write_msg(user_id, message)
         return [response, user_id]
 
+
     @bot_logger
     def favorite_list(self, user_id):
-        """Получение списка Избранного"""
+        """Получение списка Избранного:
+        - вытягивание списка избранного из БД
+        - проверкка на пустой список
+        - отправка списка"""
         favorite_list = db.favorites_list_output(user_id)
         message = ''
         if len(favorite_list) > 0:
@@ -130,9 +146,11 @@ class Bot:
         response = self._write_msg(user_id, message)
         return [response, user_id]
 
+
     def __get_user_link(self, user_id):
         """конструктор ссылки на пользователя"""
         return 'https://vk.com/' + user_id
+
 
     def __get_foto_list(self, user_id):
         """конструктор вложения с фотографиями"""
